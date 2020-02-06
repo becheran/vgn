71bb9d9e54d17a4e7808b29c3c35e44acd861601
Armin Becher <becherarmin@gmail.com>
2020-02-06 09:03:14 +0100

    Release 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vgn",
-    version="0.0.1",
+    version="0.0.2",
     author="Armin Becher",
     author_email="becherarmin@gmail.com",
     description="VGN API for python 3",
