e6d2ea20e8178debfc07e32e046afad654e7bea5
Armin Becher <armin.becher@methodpark.de>
2020-02-20 12:43:21 +0100

    Init client once

--- vgn/test_api.py
+++ vgn/test_api.py
@@ -1,37 +1,42 @@
-import asyncio
+import pytest
 from vgn.functions import *
 
-test_loop = asyncio.get_event_loop()
+
+@pytest.mark.asyncio
+async def test_api_version_did_not_change():
+    assert (await api_version()) == 'Puls-API-v1.2'
 
 
-def test_api_version_did_not_change():
-    assert test_loop.run_until_complete(api_version()) == 'Puls-API-v1.2'
+@pytest.mark.asyncio
+async def test_find_stations():
+    s = await stations("Rennweg")
+    assert len(s) > 0
 
 
-def test_find_stations():
-    assert len(test_loop.run_until_complete(stations("Rennweg"))) > 0
+@pytest.mark.asyncio
+async def test_find_not_a_station():
+    assert len(await stations("This_is_Not_a_station")) == 0
 
 
-def test_find_not_a_station():
-    assert len(test_loop.run_until_complete(stations("This_is_Not_a_station"))) == 0
+@pytest.mark.asyncio
+async def test_no_nearby_locations():
+    assert len(await nearby_stations(Coordinates(1, 1))) == 0
 
 
-def test_no_nearby_locations():
-    assert len(test_loop.run_until_complete(nearby_stations(Coordinates(1, 1)))) == 0
-
-
-def test_nearby_locations():
-    near_plaerrer = test_loop.run_until_complete(
-        nearby_stations(Coordinates(latitude=49.4480881582118, longitude=11.0647882822154), 5))
+@pytest.mark.asyncio
+async def test_nearby_locations():
+    near_plaerrer = await nearby_stations(Coordinates(latitude=49.4480881582118, longitude=11.0647882822154), 5)
     assert len(near_plaerrer) == 1
     assert near_plaerrer[0].name == 'Plärrer (Nürnberg)'
 
 
-def test_depatures():
-    dep_plaerrer = test_loop.run_until_complete(departure_schedule(704))
+@pytest.mark.asyncio
+async def test_depatures():
+    dep_plaerrer = await departure_schedule(704)
     assert len(dep_plaerrer) > 0
 
 
-def test_depatures_for_line():
-    dep_plaerrer = test_loop.run_until_complete(departure_schedule_for_line(704, 'U2'))
+@pytest.mark.asyncio
+async def test_depatures_for_line():
+    dep_plaerrer = await departure_schedule_for_line(704, 'U2')
     assert len(dep_plaerrer) > 0
